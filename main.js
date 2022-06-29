1a5c47f2c1570b1351079ec9bfc679c64e46cfff
Kimsumi <kimsumi701@gmail.com>
2022-06-29 21:54:04 +0900

    ToDoList add

--- main.js
+++ main.js
@@ -11,23 +11,62 @@
 let taskInput = document.getElementById('task-input');
 let addButton = document.getElementById('add-button');
 let taskList = [];
+let filterList = [];
+let tabs = document.querySelectorAll('.task-tabs div');
+let mode = '';
 addButton.addEventListener('click', addTask);
+
+for (let i = 1; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', function (event) {
+        filter(event);
+    });
+}
+
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);    
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false,
+    }
+    taskList.push(task);
+    // let taskContent = taskInput.value;
+    // taskList.push(taskContent);   90 
     render();
 }
 function render() {
+    let list = [];
+    if (mode == 'all') {
+        list = taskList;
+    } else if (mode == 'ongoing') {
+        list = filterList;
+    }
+
     let resultHTML = '';
-    for (let i = 0; i < taskList.length; i++) {
-        resultHTML += 
-        `<div class="task">
-        <div>${taskList[i]}</div>
-        <div>
-            <button>Check</button>
-            <button>Delete</button>
-        </div>
-        </div>`
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].isComplete == true) {
+            resultHTML += 
+            `<div class="task">
+            <div class="task-done">${list[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${list[i].id}')">Check</button>
+                <button onclick="deleteTask('${list[i].id}')">Delete</button>
+            </div>
+            </div>`
+        } else {
+            resultHTML += 
+            `<div class="task">
+            <div>${list[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${list[i].id}')">Check</button>
+                <button onclick="deleteTask('${list[i].id}')">Delete</button>
+            </div>
+            </div>`
+        }
+        /* 
+            클릭이벤트를 주는 방법 두가지
+            1. addEventListener
+            2. 해당 태그에 onclick 속성 주기 -> onclick = "함수이름()"
+        */
     }
     document.getElementById('task-board').innerHTML = resultHTML;
 }
@@ -36,4 +75,43 @@ function render() {
     innerHTML: Element의 HTML, XML을 읽어오거나, 설정할 수 있다. 태그 안에 있는 HTML 전체 내용을 들고온다.
     textContent: 해상 노드가 가지고 있는 텍스트 값을 그대로 가지고 온다. 
 */
+function toggleComplete(id) {
+    for (let i = 0; i < taskList.length; i ++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+function deleteTask(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList.splice(i, 1);
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+function filter(event) {
+    mode = event.target.id;
+    filterList = [];
+    // console.log('체크', event.target)
+    if (mode == 'all') {
+        render();
+    } else if (mode == 'ongoing') {
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].isComplete == false) {
+                filterList.push(taskList[i]);
+            }
+        }        
+        render();
+    }
+    console.log(filterList)
+}
+function randomIDGenerate() {
+    return Math.random().toString(36).substr(2, 16);
+}
 
